fc9fbc8b8512f40f2d2d71d841fa3517f07e69c4
nspencer4 <nicholas.spencer@scotts.com>
2017-10-31 13:11:06 -0400

    rename old project

--- seefood/seefood/wsgi.py
+++ seefood/seefood/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ImgurClone project.
+WSGI config for seefood project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ImgurClone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seefood.settings")
 
 application = get_wsgi_application()
