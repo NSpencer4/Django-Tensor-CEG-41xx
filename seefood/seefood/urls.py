b2ba7bed4100853d0302ef53d35c1c9f165b7375
w019ncs <spencer.119@wright.edu>
2017-11-14 14:40:58 -0500

    cleanup

--- seefood/seefood/urls.py
+++ seefood/seefood/urls.py
@@ -5,18 +5,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'seefood.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('Media.urls', namespace="Media")),
     url('^', include('django.contrib.auth.urls'))
 ]
-
-
-
-
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
