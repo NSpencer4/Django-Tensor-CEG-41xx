4f691deb46f5204757daae4fd480a3c2da0e3623
nspencer4 <nicholas.spencer@scotts.com>
2017-11-29 14:14:20 -0500

    models tweaking Posting to the upload model (not tested)

--- seefood/Media/models.py
+++ seefood/Media/models.py
@@ -7,16 +7,26 @@ from PIL import ImageOps
 import glob, os
 from django.core.urlresolvers import reverse
 
+class UploadsManager(models.Manager):
+    def create_upload(self, upload_obj):
+        upload = self.create(upload_obj)
+        # return the primary key
+        return upload
+
 class Upload(models.Model):
-    image_path = models.CharField(max_length=200)
+    image_path = models.CharField(max_length=200, null=True)
     user = models.ForeignKey(User)
-    confidence_score = models.CharField(max_length=200)
+    confidence_score = models.CharField(max_length=200, null=True)
+    tensor_verdict = models.CharField(max_length=100, null=True)
     added_on = models.DateField(auto_now_add=True)
     # This might be helpful?
-    title = models.CharField(max_length=140)
+    title = models.CharField(max_length=140, null=True)
+    accurate = models.CharField(max_length=50, null=True)
     # views = models.IntegerField(default=0)
     # upvotes = models.PositiveIntegerField(default=0)
     # downvotes = models.PositiveIntegerField(default=0)
+
+    objects = UploadsManager()
 
     def get_absolute_url(self):
         return reverse('Media:gallery', args=[str(self.id)])
--- seefood/Media/views.py
+++ seefood/Media/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Upload
 from django.http import Http404
@@ -9,6 +9,7 @@ from Media import forms, find_food
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.utils import timezone
+from datetime import datetime
 import os
 
 # Example output: C:\Users\Chase\Documents\projects\group-4110\seefood\Media
@@ -17,8 +18,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Example output: C:\Users\Chase\Documents\projects\group-4110\seefood\Media\uploads
 UPLOADS_DIR = os.path.join(BASE_DIR, 'uploads')
 
-
 def upload(request):
+    # If a user is trying to upload
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
@@ -26,19 +27,49 @@ def upload(request):
         uploaded_file_url = fs.url(filename)
         image_path = os.path.join(UPLOADS_DIR, filename)
         print ("RUNNING TENSORFLOW ON IMAGE:", image_path, ". THIS WILL TAKE A COUPLE OF MINUTES...")
+
+        # Run the image through tensorflow
         tensor_results = find_food.find_food(image_path)
+
+        # Send the Upload obj to the database
+        # Package up the necessary fields
+        upload_obj = {}
+        upload_obj['image_path'] = image_path
+        upload_obj['user'] = request.user
+        upload_obj['confidence_score'] = tensor_results['scores']
+        upload_obj['tensor_verdict'] = tensor_results['result']
+        upload_obj['title'] = 'Default Title'
+        upload_obj['accurate'] = 'Default User Accuracy'
+
+        new_upload = Upload.objects.create(image_path=upload_obj['image_path'],
+                                             added_on=datetime.utcnow(),
+                                             user=upload_obj['user'],
+                                             confidence_score=upload_obj['confidence_score'],
+                                             tensor_verdict=upload_obj['tensor_verdict'],
+                                             title=upload_obj['title'],
+                                             accurate=upload_obj['accurate'])
+
         return render(request, 'Media/upload.html', {
             'uploaded_file_url': uploaded_file_url,
-            'tensor_results': tensor_results
+            'tensor_results': tensor_results,
+            'new_upload_id': new_upload
         })
+    # Regardless of the event render the page if not done
     return render(request, 'Media/upload.html')
 
 def gallery(request):
-
+    # TODO: Change this to query by user
     context = {
-        'recent': Upload.objects.all()
+        'uploads': Upload.objects.all()
     }
+
     return render(request, 'Media/gallery.html', context)
+
+def set_accuracy(request, img_id):
+    # TODO: Fix this to allow for user input on if tensorflow was accurate or not
+    if request.method == 'PUT':
+        # Update the upload obj to see if tensorflow was accurate or not
+        img_obj = get_object_or_404(Upload, pk=img_id)
 
 def index(request):
 
@@ -47,12 +78,6 @@ def index(request):
         'loginForm': AuthenticationForm()
     }
     return render(request, 'Media/homepage.html', context)
-
-
-def random(request):
-    p = Upload.objects.order_by('?').first()
-
-    return redirect(p)
 
 def createUser(request):
     form = forms.RegistrationForm()
@@ -66,8 +91,6 @@ def createUser(request):
             return redirect(reverse('Media:loginUser'))
 
     return render(request, 'registration/createUser.html', {'form': form})
-
-
 
 def loginUser(request):
     if request.user.is_authenticated():
