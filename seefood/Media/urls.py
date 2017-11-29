a970a87d7309a8465bb22f65eff65ed7ddb6b649
nspencer4 <nicholas.spencer@scotts.com>
2017-11-29 11:55:23 -0500

    Added an admin menu link

--- seefood/Media/urls.py
+++ seefood/Media/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-
     url(r'^$', views.index, name='index'),
     url(r'^login', views.loginUser, name = "loginUser"),
     url(r'^registration', views.createUser, name ="createUser"),
